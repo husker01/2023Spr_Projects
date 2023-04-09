b41265dc654437d01abe208988f91c820c4079cf
LUWEILI <luweili71@hotmail.com>
2023-04-08 23:33:44 -0500

    CNF solver

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,6 +5,7 @@ from itertools import chain
 import random
 
 
+
 def SudokuSolver(SIZE, num_to_remove, board):
     puzzle_board = []
     mapping = {'1': 'A', '2': 'B', '3': 'C', '4': 'D', '5': 'E', '6': 'F', '7': 'G', '8': 'H', '9': 'I', '10': 'J',
